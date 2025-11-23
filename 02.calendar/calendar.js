83c4ed946926cd25159504720881df8a57affe75
shoma <s.tone@genbasupport.com>
2025-11-23 18:48:18 +0900

    dayjsを複数回呼び出すことで意図せぬ挙動を起超す可能性があったため修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,25 +5,29 @@ import minimist from "minimist";
 
 dayjs.locale("en");
 
-const ARGS = minimist(process.argv.slice(2));
-const YEAR = Object.hasOwn(ARGS, "y") ? ARGS["y"] : dayjs().format("YYYY");
-const TARGET_MONTH = Object.hasOwn(ARGS, "m")
-  ? dayjs(`${YEAR}/${ARGS["m"]}`)
-  : dayjs();
-const START_DAY = TARGET_MONTH.startOf("month");
-const END_DAY = TARGET_MONTH.endOf("month");
+var args = minimist(process.argv.slice(2));
+var dayjs_object = dayjs();
+if (Object.hasOwn(args, "y")) {
+  dayjs_object = dayjs_object.year(args["y"]);
+}
+if (Object.hasOwn(args, "m")) {
+  dayjs_object = dayjs_object.month(args["m"]);
+}
+var start_day = dayjs_object.startOf("month");
+var end_day = dayjs_object.endOf("month");
+
 const MONTH_CELL_LENGTH = 12;
 const YEAR_CELL_LENGTH = 8;
 const WEEKDAY_CELL_LENGTH = 3;
 const DAY_CELL_LENGTH = 3;
 
 process.stdout.write(
-  TARGET_MONTH.format("MMMM").padStart(MONTH_CELL_LENGTH, " "),
+  dayjs_object.format("MMMM").padStart(MONTH_CELL_LENGTH, " "),
 );
-console.log(TARGET_MONTH.format(" YYYY").padEnd(YEAR_CELL_LENGTH, " "));
+console.log(dayjs_object.format(" YYYY").padEnd(YEAR_CELL_LENGTH, " "));
 
 dayjs.extend(localeData);
-dayjs()
+dayjs_object
   .localeData()
   .weekdaysMin()
   .forEach((weekday) =>
@@ -32,12 +36,12 @@ dayjs()
 
 console.log("");
 
-for (let i = 0; i < START_DAY.day(); i++) {
+for (let i = 0; i < start_day.day(); i++) {
   process.stdout.write(" ".repeat(DAY_CELL_LENGTH));
 }
 
-for (let i = START_DAY.format("D"); i <= END_DAY.format("D"); i++) {
-  let targetDay = dayjs(`${YEAR}/${TARGET_MONTH.format("M")}/${i}`);
+for (let i = start_day.format("D"); i <= end_day.format("D"); i++) {
+  let targetDay = dayjs_object.date(i);
   if (targetDay.day() == 6) {
     console.log(targetDay.format("D").padStart(DAY_CELL_LENGTH, " "));
   } else {
