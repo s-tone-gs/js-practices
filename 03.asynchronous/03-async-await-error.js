9331875ccc41c0ccc7e2c18df31d717ee7a46074
shoma <s.tone@genbasupport.com>
2025-11-25 10:37:56 +0900

    補足したい例外のみを補足するように修正

--- 03.asynchronous/03-async-await-error.js
+++ 03.asynchronous/03-async-await-error.js
@@ -9,13 +9,17 @@ async function main() {
     var record = await db.runPromise("INSERT INTO books (title) VALUES (null)");
     console.log(`自動採番されたID:${record.lastID}`);
   } catch (err) {
-    console.log(err.message);
+    if (err.errno === 19) {
+      console.log(err.message);
+    }
   }
   try {
     var result = await db.getPromise("SELECT * FROM hogehoge LIMIT 1");
     console.log(`取得したレコード id:${result.id}, title: ${result.title}`);
   } catch (err) {
-    console.log(err.message);
+    if (err.errno === 1) {
+      console.log(err.message);
+    }
   }
   await db.runPromise("DROP TABLE books");
 }
