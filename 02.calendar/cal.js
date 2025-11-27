1f3b12e719c34fdf71f3d6e66e1ee8b195d7e1b7
shoma <s.tone@genbasupport.com>
2025-11-27 14:32:59 +0900

    変数の中身が分かる命名に修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,28 +9,26 @@ import minimist from "minimist";
 dayjs.locale("en");
 
 let args = minimist(process.argv.slice(2));
-let dayjs_object = dayjs();
+let dateTime = dayjs();
 if (Object.hasOwn(args, "y")) {
-  dayjs_object = dayjs_object.year(args["y"]);
+  dateTime = dateTime.year(args["y"]);
 }
 if (Object.hasOwn(args, "m")) {
   // monthはゼロインデックス(0~11)だが、引数は1~12を受け取るため-1している
-  dayjs_object = dayjs_object.month(args["m"] - 1);
+  dateTime = dateTime.month(args["m"] - 1);
 }
-let start_day = dayjs_object.startOf("month");
-let end_day = dayjs_object.endOf("month");
+let start_day = dateTime.startOf("month");
+let end_day = dateTime.endOf("month");
 
 const MONTH_CELL_LENGTH = 12;
 const YEAR_CELL_LENGTH = 8;
 const WEEKDAY_CELL_LENGTH = 2;
 const DAY_CELL_LENGTH = 2;
 
-process.stdout.write(
-  dayjs_object.format("MMMM").padStart(MONTH_CELL_LENGTH, " "),
-);
-console.log(dayjs_object.format(" YYYY").padEnd(YEAR_CELL_LENGTH, " "));
+process.stdout.write(dateTime.format("MMMM").padStart(MONTH_CELL_LENGTH, " "));
+console.log(dateTime.format(" YYYY").padEnd(YEAR_CELL_LENGTH, " "));
 
-dayjs_object
+dateTime
   .localeData()
   .weekdaysMin()
   .forEach((weekday) => {
@@ -46,7 +44,7 @@ for (let i = 0; i < start_day.day(); i++) {
 }
 
 for (let i = start_day.format("D"); i <= end_day.format("D"); i++) {
-  let targetDay = dayjs_object.date(i);
+  let targetDay = dateTime.date(i);
   if (targetDay.day() == 6) {
     process.stdout.write(targetDay.format("D").padStart(DAY_CELL_LENGTH, " "));
     console.log(" ");
