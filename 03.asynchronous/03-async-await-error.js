55ae6af9c46b534ca83b392a2f8c3ee074209353
shoma <s.tone@genbasupport.com>
2025-11-28 14:10:51 +0900

    Top-level awaitを使用

--- 03.asynchronous/03-async-await-error.js
+++ 03.asynchronous/03-async-await-error.js
@@ -4,31 +4,25 @@ import {
   getPromise,
 } from "./database-helper.js";
 
-async function main() {
-  let db = await openDatabasePromise(":memory:");
-  await runPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-  );
-  try {
-    let record = await runPromise(
-      db,
-      "INSERT INTO books (title) VALUES (null)",
-    );
-    console.log(`自動採番されたID:${record.lastID}`);
-  } catch (err) {
-    if (err.errno === 19) {
-      console.log(err.message);
-    }
+let db = await openDatabasePromise(":memory:");
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+);
+try {
+  let record = await runPromise(db, "INSERT INTO books (title) VALUES (null)");
+  console.log(`自動採番されたID:${record.lastID}`);
+} catch (err) {
+  if (err.errno === 19) {
+    console.log(err.message);
   }
-  try {
-    let result = await getPromise(db, "SELECT * FROM hogehoge LIMIT 1");
-    console.log(`取得したレコード id:${result.id}, title: ${result.title}`);
-  } catch (err) {
-    if (err.errno === 1) {
-      console.log(err.message);
-    }
-  }
-  await runPromise(db, "DROP TABLE books");
 }
-main();
+try {
+  let result = await getPromise(db, "SELECT * FROM hogehoge LIMIT 1");
+  console.log(`取得したレコード id:${result.id}, title: ${result.title}`);
+} catch (err) {
+  if (err.errno === 1) {
+    console.log(err.message);
+  }
+}
+await runPromise(db, "DROP TABLE books");
--- 03.asynchronous/03-async-await-no-error.js
+++ 03.asynchronous/03-async-await-no-error.js
@@ -4,19 +4,13 @@ import {
   getPromise,
 } from "./database-helper.js";
 
-async function main() {
-  let db = await openDatabasePromise(":memory:");
-  await runPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-  );
-  let record = await runPromise(
-    db,
-    "INSERT INTO books (title) VALUES ('test')",
-  );
-  console.log(`自動採番されたID:${record.lastID}`);
-  let result = await getPromise(db, "SELECT * FROM books LIMIT 1");
-  console.log(`取得したレコード id:${result.id}, title: ${result.title}`);
-  await runPromise(db, "DROP TABLE books");
-}
-main();
+let db = await openDatabasePromise(":memory:");
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+);
+let record = await runPromise(db, "INSERT INTO books (title) VALUES ('test')");
+console.log(`自動採番されたID:${record.lastID}`);
+let result = await getPromise(db, "SELECT * FROM books LIMIT 1");
+console.log(`取得したレコード id:${result.id}, title: ${result.title}`);
+await runPromise(db, "DROP TABLE books");
