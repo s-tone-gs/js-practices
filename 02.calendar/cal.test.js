384bac77ee634b4e63c5aa80b1ef0b03b439c83c
shoma <s.tone@genbasupport.com>
2025-12-08 11:26:07 +0900

    オプションなしの場合と月だけを指定した場合のテストを追加&テストの命名を修正

--- 02.calendar/cal.test.js
+++ 02.calendar/cal.test.js
@@ -1,8 +1,51 @@
 import { expect, test } from "vitest";
+import MockDate from "mockdate";
 import { outdent } from "outdent";
 import { buildCalendar } from "./cal_method.js";
 
+test("no option", () => {
+  MockDate.set("2025-12-1");
+  const expected = outdent`
+  ${outdent}
+     December 2025
+  Su Mo Tu We Th Fr Sa
+      1  2  3  4  5  6
+   7  8  9 10 11 12 13
+  14 15 16 17 18 19 20
+  21 22 23 24 25 26 27
+  28 29 30 31`;
+  expect(buildCalendar({})).toBe(expected);
+});
+
+test("option only month use median value of month", () => {
+  MockDate.set("2025-7-1");
+  const expected = outdent`
+  ${outdent}
+       July 2025
+  Su Mo Tu We Th Fr Sa
+         1  2  3  4  5
+   6  7  8  9 10 11 12
+  13 14 15 16 17 18 19
+  20 21 22 23 24 25 26
+  27 28 29 30 31`;
+  expect(buildCalendar({ month: 6 })).toBe(expected);
+});
+
+test("option year and month use median value of yearMonth", () => {
+  const expected = outdent`
+    ${outdent}
+         July 2035
+    Su Mo Tu We Th Fr Sa
+     1  2  3  4  5  6  7
+     8  9 10 11 12 13 14
+    15 16 17 18 19 20 21
+    22 23 24 25 26 27 28
+    29 30 31`;
+  expect(buildCalendar({ year: 2035, month: 6 })).toBe(expected);
+});
+
 test("minimum value of month", () => {
+  MockDate.set("2025");
   const expected = outdent`
     ${outdent}
         January 2025
@@ -12,23 +55,11 @@ test("minimum value of month", () => {
     12 13 14 15 16 17 18
     19 20 21 22 23 24 25
     26 27 28 29 30 31`;
-  expect(buildCalendar({ year: 2025, month: 0 })).toBe(expected);
-});
-
-test("median of month", () => {
-  const expected = outdent`
-    ${outdent}
-         July 2025
-    Su Mo Tu We Th Fr Sa
-           1  2  3  4  5
-     6  7  8  9 10 11 12
-    13 14 15 16 17 18 19
-    20 21 22 23 24 25 26
-    27 28 29 30 31`;
-  expect(buildCalendar({ year: 2025, month: 6 })).toBe(expected);
+  expect(buildCalendar({ month: 0 })).toBe(expected);
 });
 
 test("maximum value of month", () => {
+  MockDate.set("2025");
   const expected = outdent`
     ${outdent}
        December 2025
@@ -38,7 +69,7 @@ test("maximum value of month", () => {
     14 15 16 17 18 19 20
     21 22 23 24 25 26 27
     28 29 30 31`;
-  expect(buildCalendar({ year: 2025, month: 11 })).toBe(expected);
+  expect(buildCalendar({ month: 11 })).toBe(expected);
 });
 
 test("minimum value of year", () => {
@@ -52,19 +83,6 @@ test("minimum value of year", () => {
   18 19 20 21 22 23 24
   25 26 27 28 29 30 31`;
   expect(buildCalendar({ year: 1970, month: 0 })).toBe(expected);
-});
-
-test("median of year", () => {
-  const expected = outdent`
-  ${outdent}
-      January 2035
-  Su Mo Tu We Th Fr Sa
-      1  2  3  4  5  6
-   7  8  9 10 11 12 13
-  14 15 16 17 18 19 20
-  21 22 23 24 25 26 27
-  28 29 30 31`;
-  expect(buildCalendar({ year: 2035, month: 0 })).toBe(expected);
 });
 
 test("maximum value of yaer", () => {
