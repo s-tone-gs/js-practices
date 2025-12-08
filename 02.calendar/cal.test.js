be1f11d0c42b7b804f042feee5cf7ccad956ee64
shoma <s.tone@genbasupport.com>
2025-12-08 19:23:34 +0900

    期待値のインデントを統一

--- 02.calendar/cal.test.js
+++ 02.calendar/cal.test.js
@@ -6,28 +6,28 @@ import { buildCalendar } from "./cal_method.js";
 test("no option", () => {
   MockDate.set("2025-12-1");
   const expected = outdent`
-  ${outdent}
-     December 2025
-  Su Mo Tu We Th Fr Sa
-      1  2  3  4  5  6
-   7  8  9 10 11 12 13
-  14 15 16 17 18 19 20
-  21 22 23 24 25 26 27
-  28 29 30 31`;
+    ${outdent}
+       December 2025
+    Su Mo Tu We Th Fr Sa
+        1  2  3  4  5  6
+     7  8  9 10 11 12 13
+    14 15 16 17 18 19 20
+    21 22 23 24 25 26 27
+    28 29 30 31`;
   expect(buildCalendar({})).toBe(expected);
 });
 
 test("month-only option using the monthly median", () => {
   MockDate.set("2025-7-1");
   const expected = outdent`
-  ${outdent}
-       July 2025
-  Su Mo Tu We Th Fr Sa
-         1  2  3  4  5
-   6  7  8  9 10 11 12
-  13 14 15 16 17 18 19
-  20 21 22 23 24 25 26
-  27 28 29 30 31`;
+    ${outdent}
+         July 2025
+    Su Mo Tu We Th Fr Sa
+           1  2  3  4  5
+     6  7  8  9 10 11 12
+    13 14 15 16 17 18 19
+    20 21 22 23 24 25 26
+    27 28 29 30 31`;
   expect(buildCalendar({ month: 6 })).toBe(expected);
 });
 
@@ -74,27 +74,27 @@ test("maximum value of month", () => {
 
 test("minimum value of year", () => {
   const expected = outdent`
-  ${outdent}
-      January 1970
-  Su Mo Tu We Th Fr Sa
-               1  2  3
-   4  5  6  7  8  9 10
-  11 12 13 14 15 16 17
-  18 19 20 21 22 23 24
-  25 26 27 28 29 30 31`;
+    ${outdent}
+        January 1970
+    Su Mo Tu We Th Fr Sa
+                 1  2  3
+     4  5  6  7  8  9 10
+    11 12 13 14 15 16 17
+    18 19 20 21 22 23 24
+    25 26 27 28 29 30 31`;
   expect(buildCalendar({ year: 1970, month: 0 })).toBe(expected);
 });
 
 test("maximum value of year", () => {
   const expected = outdent`
-  ${outdent}
-      January 2100
-  Su Mo Tu We Th Fr Sa
-                  1  2
-   3  4  5  6  7  8  9
-  10 11 12 13 14 15 16
-  17 18 19 20 21 22 23
-  24 25 26 27 28 29 30
-  31`;
+    ${outdent}
+        January 2100
+    Su Mo Tu We Th Fr Sa
+                    1  2
+     3  4  5  6  7  8  9
+    10 11 12 13 14 15 16
+    17 18 19 20 21 22 23
+    24 25 26 27 28 29 30
+    31`;
   expect(buildCalendar({ year: 2100, month: 0 })).toBe(expected);
 });
