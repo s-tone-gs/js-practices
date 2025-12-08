9619f5649ea2692a9ce863a47350d820db312c02
shoma <s.tone@genbasupport.com>
2025-12-08 09:50:48 +0900

    テストの意図が伝わりづらい実装であったため修正

--- 02.calendar/cal.test.js
+++ 02.calendar/cal.test.js
@@ -1,95 +1,82 @@
-import { buildCalendar } from "./cal_method.js";
 import { expect, test } from "vitest";
+import { outdent } from "outdent";
+import { buildCalendar } from "./cal_method.js";
 
-const LEADING_LINE_BREAK = /^[\n]+/;
-
-test("calendar 2025/1", () => {
-  const expected = `
-    January 2025
-Su Mo Tu We Th Fr Sa
-          1  2  3  4
- 5  6  7  8  9 10 11
-12 13 14 15 16 17 18
-19 20 21 22 23 24 25
-26 27 28 29 30 31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("minimum value of month", () => {
+  const expected = outdent`
+    ${outdent}
+        January 2025
+    Su Mo Tu We Th Fr Sa
+              1  2  3  4
+     5  6  7  8  9 10 11
+    12 13 14 15 16 17 18
+    19 20 21 22 23 24 25
+    26 27 28 29 30 31`;
   expect(buildCalendar(2025, 1)).toBe(expected);
 });
 
-test("calendar 2025/7", () => {
-  const expected = `
-     July 2025
-Su Mo Tu We Th Fr Sa
-       1  2  3  4  5
- 6  7  8  9 10 11 12
-13 14 15 16 17 18 19
-20 21 22 23 24 25 26
-27 28 29 30 31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("median of month", () => {
+  const expected = outdent`
+    ${outdent}
+         July 2025
+    Su Mo Tu We Th Fr Sa
+           1  2  3  4  5
+     6  7  8  9 10 11 12
+    13 14 15 16 17 18 19
+    20 21 22 23 24 25 26
+    27 28 29 30 31`;
   expect(buildCalendar(2025, 7)).toBe(expected);
 });
 
-test("calendar 2025/12", () => {
-  const expected = `
-   December 2025
-Su Mo Tu We Th Fr Sa
-    1  2  3  4  5  6
- 7  8  9 10 11 12 13
-14 15 16 17 18 19 20
-21 22 23 24 25 26 27
-28 29 30 31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("maximum value of month", () => {
+  const expected = outdent`
+    ${outdent}
+       December 2025
+    Su Mo Tu We Th Fr Sa
+        1  2  3  4  5  6
+     7  8  9 10 11 12 13
+    14 15 16 17 18 19 20
+    21 22 23 24 25 26 27
+    28 29 30 31`;
   expect(buildCalendar(2025, 12)).toBe(expected);
 });
 
-test("calendar 1970/1", () => {
-  const expected = `
-    January 1970
-Su Mo Tu We Th Fr Sa
-             1  2  3
- 4  5  6  7  8  9 10
-11 12 13 14 15 16 17
-18 19 20 21 22 23 24
-25 26 27 28 29 30 31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("minimum value of year", () => {
+  const expected = outdent`
+  ${outdent}
+      January 1970
+  Su Mo Tu We Th Fr Sa
+               1  2  3
+   4  5  6  7  8  9 10
+  11 12 13 14 15 16 17
+  18 19 20 21 22 23 24
+  25 26 27 28 29 30 31`;
   expect(buildCalendar(1970, 1)).toBe(expected);
 });
 
-test("calendar 2035/1", () => {
-  const expected = `
-    January 2035
-Su Mo Tu We Th Fr Sa
-    1  2  3  4  5  6
- 7  8  9 10 11 12 13
-14 15 16 17 18 19 20
-21 22 23 24 25 26 27
-28 29 30 31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("median of year", () => {
+  const expected = outdent`
+  ${outdent}
+      January 2035
+  Su Mo Tu We Th Fr Sa
+      1  2  3  4  5  6
+   7  8  9 10 11 12 13
+  14 15 16 17 18 19 20
+  21 22 23 24 25 26 27
+  28 29 30 31`;
   expect(buildCalendar(2035, 1)).toBe(expected);
 });
 
-test("calendar 2100/1", () => {
-  const expected = `
-    January 2100
-Su Mo Tu We Th Fr Sa
-                1  2
- 3  4  5  6  7  8  9
-10 11 12 13 14 15 16
-17 18 19 20 21 22 23
-24 25 26 27 28 29 30
-31
-  `
-    .trimEnd()
-    .replace(LEADING_LINE_BREAK, "");
+test("maximum value of yaer", () => {
+  const expected = outdent`
+  ${outdent}
+      January 2100
+  Su Mo Tu We Th Fr Sa
+                  1  2
+   3  4  5  6  7  8  9
+  10 11 12 13 14 15 16
+  17 18 19 20 21 22 23
+  24 25 26 27 28 29 30
+  31`;
   expect(buildCalendar(2100, 1)).toBe(expected);
 });
