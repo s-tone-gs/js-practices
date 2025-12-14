33438190d960a0937ee041a08eafa548a66ba6ba
shoma <s.tone@genbasupport.com>
2025-12-15 08:25:31 +0900

    typoを修正

--- 03.asynchronous/database-helper.js
+++ 03.asynchronous/database-helper.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 
 export function runPromise(db, sql, params = []) {
   return new Promise((resolve, reject) => {
-    db.run(sql, param, function (err) {
+    db.run(sql, params, function (err) {
       if (err) {
         reject(err);
       } else {
@@ -14,7 +14,7 @@ export function runPromise(db, sql, params = []) {
 
 export function getPromise(db, sql, params = []) {
   return new Promise((resolve, reject) => {
-    db.get(sql, param, (err, row) => {
+    db.get(sql, params, (err, row) => {
       if (err) {
         reject(err);
       } else {
