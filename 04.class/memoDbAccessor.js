d3f2c33b1d0d7c25c3ff72ae393aca30b5e05171
shoma <s.tone@genbasupport.com>
2026-01-29 11:13:32 +0900

    fix: 外部から使用されない値をプライベートフィールドに変更

--- 04.class/memoDbAccessor.js
+++ 04.class/memoDbAccessor.js
@@ -2,22 +2,25 @@ import * as sqliteWrapper from "./sqliteWrapper.js";
 import Memo from "./memoClass.js";
 
 export default class MemoDbAccessor {
+  #connectedDb;
+  #tableName;
+
   constructor(connectedDb) {
-    this.connectedDb = connectedDb;
-    this.tableName = "memos";
+    this.#connectedDb = connectedDb;
+    this.#tableName = "memos";
   }
 
   async ensureTableExists() {
     await sqliteWrapper.run(
-      this.connectedDb,
-      `CREATE TABLE IF NOT EXISTS ${this.tableName} (id INTEGER PRIMARY KEY, content text NOT NULL)`,
+      this.#connectedDb,
+      `CREATE TABLE IF NOT EXISTS ${this.#tableName} (id INTEGER PRIMARY KEY, content text NOT NULL)`,
     );
   }
 
   async all() {
     const memos = await sqliteWrapper.all(
-      this.connectedDb,
-      `SELECT * FROM ${this.tableName} ORDER BY id ASC`,
+      this.#connectedDb,
+      `SELECT * FROM ${this.#tableName} ORDER BY id ASC`,
     );
     return memos.map(
       ({ id, content }) =>
@@ -30,16 +33,16 @@ export default class MemoDbAccessor {
 
   async delete(id) {
     await sqliteWrapper.run(
-      this.connectedDb,
-      `DELETE FROM ${this.tableName} WHERE id = ?`,
+      this.#connectedDb,
+      `DELETE FROM ${this.#tableName} WHERE id = ?`,
       [id],
     );
   }
 
   async save(content) {
     await sqliteWrapper.run(
-      this.connectedDb,
-      `INSERT INTO ${this.tableName} (content) VALUES (?)`,
+      this.#connectedDb,
+      `INSERT INTO ${this.#tableName} (content) VALUES (?)`,
       [content],
     );
   }
